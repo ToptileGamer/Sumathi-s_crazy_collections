aa34ceadf51bb147de07634cac4924c123290e7b
ToptileGamer <jgautham42@gmail.com>
2026-05-23 20:39:39 +0530

    settings

--- Sumathi's_Crazy_Collections/src/pages/Profile.jsx
+++ Sumathi's_Crazy_Collections/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { useWishlist } from "../hooks/useWishlist";
 import { signOut, updateProfile } from "../services/authService";
